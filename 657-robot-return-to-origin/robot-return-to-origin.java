29b5e9f3acd94d4854698899c509ebc76e45732f
Shiva Kumar Keesari <keesarishivakumar50@gmail.com>
2025-10-31 15:10:12 +0530

    Time: 4 ms (96.70%) | Memory: 46.2 MB (6.39%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean judgeCircle(String moves) {
         int x=0,y=0;
-        for(int i=0;i<moves.length();i++){
-            char ch=moves.charAt(i);
+        for(char ch:moves.toCharArray()){
             if(ch=='U') y+=1;
             else if(ch=='D') y-=1;
             else if(ch=='L') x+=1;
