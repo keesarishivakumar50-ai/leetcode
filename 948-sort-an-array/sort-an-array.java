7cc6b0a85ccd6c13a0e696b0cfe44f7fbfe873ea
Shiva Kumar Keesari <keesarishivakumar50@gmail.com>
2026-02-27 14:02:05 +0530

    Time: 28 ms (43.68%) | Memory: 83.3 MB (56.10%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int left = 0 , right = nums.length - 1;
+        Divide(nums , left , right);
+        return nums;
+    }
+    void Divide(int arr[] , int left , int right){
+        if(left == right) return;
+        int mid = left + (right - left) / 2;
+        Divide(arr , left , mid);
+        Divide(arr , mid + 1, right);
+        merge(arr , left , mid , right);
+    }
+
+    void merge(int arr[] , int left , int mid , int right){
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        
+        for(int i = 0 ; i < n1 ; i++){
+            L[i] = arr[left + i];
+        }
+        
+        for(int i = 0 ; i < n2 ; i++){
+            R[i] = arr[mid + 1 + i];
+        }
+        
+        int i = 0 , j = 0 , k = left;
+        while(i < n1 && j < n2){
+            if(L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
+            }
+            else{
+                arr[k] = R[j++];
+            }
+            k++;
+        }
+        while(i < n1){
+            arr[k++] = L[i++];
+        }
+        while(j < n2){
+            arr[k++] = R[j++];
+        }
+    }
+}
