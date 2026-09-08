bdcd73cab3c2fa891ee8c0fc646a65f838b6146d
Shiva Kumar Keesari <keesarishivakumar50@gmail.com>
2026-09-08 20:13:30 +0530

    Time: 2 ms (97.73%) | Memory: 72.3 MB (68.99%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0 , ten = 0;
+        for(int num : bills){
+            if(num == 5) five++;
+            else if(num == 10){
+                if(five > 0){
+                    five--;
+                    ten++;
+                }
+                else return false;
+            }
+            else if(num == 20){
+                if(ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                }
+                else if(five > 0 && ten == 0){
+                    if(five > 2) five = five - 3;
+                    else return false;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
